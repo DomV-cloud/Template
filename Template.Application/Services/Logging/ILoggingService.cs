086e38947dd6099343cf344ef65d645e47307601
Petr Barabáš <petr.barabas@memos.cz>
2024-11-11 18:30:49 +0100

    added logging service

---
+++ Template.Application/Services/Logging/ILoggingService.cs
@@ -0,0 +1,9 @@
+﻿namespace Template.Application.Services.Logging
+{
+    public interface ILoggingService
+    {
+        Task LogInformationAsync(string message, params object?[] args);
+        Task LogWarningAsync(string message, params object?[] args);
+        Task LogErrorAsync(string message, params object?[] args);
+    }
+}
---
+++ Template.Application/Services/Logging/LoggingService.cs
@@ -0,0 +1,41 @@
+﻿namespace Template.Application.Services.Logging
+{
+    public class LoggingService : ILoggingService
+    {
+        private readonly string _logFilePath;
+
+        public LoggingService()
+        {
+            _logFilePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Logs", "log.txt");
+
+            var logDirectory = Path.GetDirectoryName(_logFilePath);
+            if (!Directory.Exists(logDirectory))
+            {
+                Directory.CreateDirectory(logDirectory);
+            }
+        }
+
+        public Task LogInformationAsync(string message, params object?[] args)
+        {
+            return LogAsync("INFO", message, args);
+        }
+
+        public Task LogWarningAsync(string message, params object?[] args)
+        {
+            return LogAsync("WARN", message, args);
+        }
+
+        public Task LogErrorAsync(string message, params object?[] args)
+        {
+            return LogAsync("ERROR", message, args);
+        }
+
+        private async Task LogAsync(string logLevel, string message, params object?[] args)
+        {
+            var formattedMessage = string.Format(message, args);
+            var logMessage = $"{DateTime.UtcNow:yyyy-MM-dd HH:mm:ss} [{logLevel}] {formattedMessage}";
+            using var writer = new StreamWriter(_logFilePath, true);
+            await writer.WriteLineAsync(logMessage);
+        }
+    }
+}
